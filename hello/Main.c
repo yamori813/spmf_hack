c33d70ee800f6807aa87af396dabbbbde706075b
Hiroki Mori <yamori813@yahoo.co.jp>
2024-01-24 16:36:38 +0900

    revert stack and add put

--- hello/Main.c
+++ hello/Main.c
@@ -1,10 +1,15 @@
 int main(void)
 {
 long *ptr;
+unsigned char *ptr8;
+
 	ptr = (long *)0xb0004000;
 
 	print("Hello World!!");
 	printf("MORIMORI %d\n", 1234);
 	printf("MORIMORI %x\n", *ptr);
+	put('m');
+	aput('m');
+
 	for(;;) ;
 }
