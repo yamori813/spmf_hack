67ee71d580c7d274004d3af2d1284a159637292d
Hiroki Mori <yamori813@yahoo.co.jp>
2024-01-29 07:51:05 +0900

    abandon

--- hello/Main.c
+++ hello/Main.c
@@ -1,3 +1,41 @@
+void initlcd()
+{
+unsigned char *ptr8;
+unsigned char c;
+
+	ptr8 = (unsigned char *)0xb000103c;
+	c = *ptr8;
+	c &= -10;
+	*ptr8 = c;
+
+	ptr8 = (unsigned char *)0xb0001034;
+	c = *ptr8;
+	c &= -2;
+	*ptr8 = c;
+	c |= 8;
+	*ptr8 = c;
+
+	ptr8 = (unsigned char *)0xb0001030;
+	c = *ptr8;
+	c |= 8;
+	*ptr8 = c;
+
+	ptr8 = (unsigned char *)0xb000a108;
+	c = *ptr8;
+	c |= -128;
+	*ptr8 = c;
+
+	ptr8 = (unsigned char *)0xb000a114;
+	c = *ptr8;
+	c |= -128;
+	*ptr8 = c;
+
+	ptr8 = (unsigned char *)0xb000a110;
+	c = *ptr8;
+	c |= -128;
+	*ptr8 = c;
+}
+
 int main(void)
 {
 long *ptr;
@@ -7,8 +45,12 @@ long *ptr;
 	print("Hello World!!");
 	printf("MORIMORI %d\n", 1234);
 	printf("MORIMORI %x\n", *ptr);
-	put('m');
-	aput('a');
+	printf("MORIMORI %x\n", rd_count());
+	printf("MORIMORI %x\n", get());
+
+	initlcd();
+
+	put('.');
 
 	for(;;) ;
 }
--- hello/mkimg.sh
+++ hello/mkimg.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-dd if=../../original.bin of=boot.bin bs=64k count=25 >/dev/null 2>&1
+make
+#dd if=../../original.bin of=boot.bin bs=64k count=25 >/dev/null 2>&1
 ../tools/splzo Kernel.bin Kernel.lzo
-cat boot.bin Kernel.lzo > tmp.bin
+#cat boot.bin Kernel.lzo > tmp.bin
+cat ../../bootonly.bin Kernel.lzo > tmp.bin
 dd if=tmp.bin of=2m.bin ibs=2m conv=sync >/dev/null 2>&1
-rm boot.bin tmp.bin 
+#rm boot.bin tmp.bin 
+scp 2m.bin 10.0.1.37:
