6936fd27ff0037831c4baa283cda779fd115b318
Hiroki Mori <yamori813@yahoo.co.jp>
2026-01-21 15:32:46 +0900

    delete not use function

--- hello/Main2.c
+++ hello/Main2.c
@@ -1,7 +1,5 @@
 #include <string.h>
 
-extern char _end[];
-
 void myprint(char *ch)
 {
 	while(*ch) {
@@ -31,17 +29,3 @@ char str[128];
 	}
 }
 
-void *
-_sbrk (incr)
-     int incr;
-{
-   static char * heap_end = _end;
-   char *        prev_heap_end;
-
-   prev_heap_end = heap_end;
-   heap_end += incr;
-
-   return (void *) prev_heap_end;
-}
-
-char * sbrk (int) __attribute__((weak, alias ("_sbrk")));
