13938da5c4ee71d313dfc6936acb03e651f9499c
Hiroki Mori <yamori813@yahoo.co.jp>
2026-01-21 21:24:09 +0900

    add prid cop0 access

--- hello/Main.c
+++ hello/Main.c
@@ -7,7 +7,7 @@ long *ptr;
 	print("Hello World!!");
 	printf("MORIMORI %d\n", 1234);
 	printf("MORIMORI %x\n", *ptr);
-	printf("MORIMORI %x\n", rd_count());
+	printf("Company ID %d\n", (rd_prid() >> 16) & 0xff);
 	printf("MORIMORI %x\n", get());
 
 	put('.');
