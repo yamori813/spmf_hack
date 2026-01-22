902143a5d859548de4e0087365051b3f21947ece
Hiroki Mori <yamori813@yahoo.co.jp>
2026-01-22 16:28:33 +0900

    separete boot function call

--- hello/Main.c
+++ hello/Main.c
@@ -4,11 +4,12 @@ long *ptr;
 
 	ptr = (long *)0xb0004000;
 
-	print("Hello World!!");
-	printf("MORIMORI %d\n", 1234);
-	printf("MORIMORI %x\n", *ptr);
-	printf("Company ID %d\n", (rd_prid() >> 16) & 0xff);
-	printf("MORIMORI %x\n", get());
+	bfprint("Hello World!!");
+	bfprintf("MORIMORI %d\n", 1234);
+	bfprintf("MORIMORI %x\n", *ptr);
+	bfprintf("Company ID %d\n", (rd_prid() >> 16) & 0xff);
+	bfprintf("MORIMORI %x\n", get());
+	try();
 
 	put('.');
 
