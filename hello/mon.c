0229124f1b6c3b717b052257a076b2341203906a
Hiroki Mori <yamori813@yahoo.co.jp>
2026-01-24 13:32:08 +0900

    add monitor

---
+++ hello/mon.c
@@ -0,0 +1,87 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "xprintf.h"
+
+void myprint(char *ch)
+{
+	while(*ch) {
+		cput((unsigned char *)0xb0000000, *ch);
+		++ch;
+	}
+}
+
+void hprint(int n)
+{
+int i, t;
+	for (i = 0; i < 8; ++i) {
+		t = (n >> (4 * (7 - i))) & 0xf;
+		if (t >= 10)
+			cput((unsigned char *)0xb0000000, t - 10 + 'a');
+		else
+			cput((unsigned char *)0xb0000000, t + '0');
+	}
+}
+
+unsigned int htoi(char *buf)
+{
+unsigned int res, n;
+
+	n = 0;
+	res = 0;
+	while(buf[n] != 0) {
+		if ((buf[n] <= 'f' && buf[n] >= 'a') ||
+		    (buf[n] <= '9' && buf[n] >= '0')) {
+			res <<= 4;
+			if (buf[n] >= 'a') {
+				res += buf[n] - 'a' + 10;
+			} else {
+				res += buf[n] - '0';
+			}
+		}
+		++n;
+	}
+
+	return res;
+}
+
+int main(void)
+{
+int i, n;
+char ch;
+char cmd[128];
+char str[128];
+int *addr;
+
+	myprint(" spmf2800 monitor start\r\n");
+	myprint("> ");
+
+	n = 0;
+	for(;;) {
+		if (cget(&ch)) {
+			cmd[n] = ch;
+			cput((unsigned char *)0xb0000000, cmd[n]);
+			++n;
+			if (ch == '\r') {
+				cput((unsigned char *)0xb0000000, '\n');
+				cmd[n - 1] = 0;
+				if (cmd[0] == 'D') {
+					addr = (int *)htoi(cmd + 2);
+					hprint((int)addr);
+					myprint(" ");
+					for (i = 0; i < 16; i += 4) {
+						myprint(" ");
+						hprint(*(addr + i));
+					}
+					myprint("\r\n");
+				} else if (cmd[0] == 'G') {
+					addr = (int *)htoi(cmd + 2);
+					jmp(addr);
+				} else
+					myprint("error\r\n");
+				n = 0;	
+				myprint("> ");
+			}
+		}
+	}
+}
